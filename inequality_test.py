c84a6d5af6b5cfc8947be07411d5598edb556405
Timothy Zer-An Chu <tzchu@scs.ad.cs.cmu.edu>
2019-05-27 09:15:57 -0400

    Tested a range of inequaities related to distance matrices. Most were false, some things seemed vaguely true (related to L_p norms being concave for p < 1)

---
+++ inequality_test.py
@@ -0,0 +1,210 @@
+import numpy as np
+from numpy.linalg import inv
+from screw import screw
+""" Tested a number of inequalities. Starting with:
+ Whether for Euclidean distance matrices (hereafter referred to as distance matrices):
+ 1. Radius(D1)+Radius(D2) <= 2*Radius(D1 + D2 / 2). 
+ 	Results: False (dist_inequality)
+ 2. Whether a^T A a + b^T B b <= 2*(a+b)/2 ^T (A+B)/2 (a+b)/2
+ 	Results: False (big_concavity_test)
+ 3. Tested some p-norm inequality for diagonal matrices. See val, inequality for specs.
+ 4. Tested whether distance matrices were preserved under harmonic mean. This was false. (See Harmonic, neg_type)
+ """
+
+def rand_3_dist():
+	return n_rand_dist(3)
+def n_rand_dist(n):
+	# Tends to generate things like a simplex.
+	a = np.random.rand(n,n-1)
+	A = np.zeros((n,n))
+	for i in range(n):
+		for j in range(n):
+			A[i][j] = np.linalg.norm(a[i]-a[j])**2
+	return A
+def radius_thing(D):
+	n = len(D)
+	ones = np.ones(n)
+	two_rad_squared = 1/(ones @ np.linalg.inv(D) @ ones) # You need to invert to get the radius
+	b = np.linalg.inv(D)@ones*two_rad_squared
+	print ("Barycentric Coords are:")
+	return two_rad_squared
+
+# concavity Test:
+# Test whether a^T A a + b^T B b <= 2* (a+b)/2^T (A+B/2)(a+b)/2
+# Result: fails, for A and B from distance matrix |i-j|. See big_concavity_test
+def concavity_test(a, A, b, B):
+	concavity_val =  a @ A @ a + b @ B @ b - 2 * (a+b)/2 @ (A+B)/2 @ (a+b)/2
+	return concavity_val
+
+# Even for screw distance with edge-squared metric, Fails this concavity test.
+def big_concavity_test():
+	for i in range(100):	
+		ls = np.sort(np.random.rand(3))
+		A = screw(ls, 1)
+		ls2 = np.sort(np.random.rand(3))
+		B = screw(ls2, 1)
+		(a,b) = np.random.rand(3), np.random.rand(3)
+		(a,b) = a/sum(a), b/sum(b)
+		print(i)
+		if (concavity_test(a, A, b, B) >= 0.00001):
+			print("A: ", A)
+			print("a: ", a)
+			print("B: ", B)
+			print("b: ", b)
+			print("Avg: ", (A+B)/2)
+			print("a+b/2: ", (a+b)/2)
+			print(a @ A @ a)
+			print(b @ B @ b)
+			print((a+b)/2 @ (A+B)/2 @ (a+b)/2)
+			print("Concavity Value:", concavity_test(a,A,b,B))
+			print("Fails second concavity test")
+			return False
+	return True
+# I attempted to test whether if D is negative type, then 
+# max(x, y, z, orthogonal to ones) x^T D1 x + y^T D2 y <= z^T avg(D1, D2) z
+# It seems always false :D :D :D :D :D :D :D :D.
+def dist_inequality(p):
+	D1 = rand_3_dist()
+	D2 = rand_3_dist()
+	D3 = (D1+D2)/2
+	print(D1)
+	print(D2)
+	print(D3)
+	rad_D1 = radius_thing(D1)
+	rad_D2 = radius_thing(D2)
+	rad_D3 = radius_thing(D3)
+	print (rad_D1+rad_D2-2*rad_D3)
+	return (rad_D1+rad_D1-2*rad_D3 <= 0.000001)
+
+# Test for diagonal matrix with two entries.
+# It seems to work.
+def val(a, b, p):
+	av = pow(a, 1/p)
+	bv = pow(b, 1/p)
+	# Minimal value of ax^2 + by^2 when x+y=1
+	v = (2*av*bv)/(av+bv)
+	# Inverted powered v should be the circumradius
+	return pow(1/v, p)
+
+def inequality(a, b, c, d, p = 3/2):
+	firstval = val(a,b,p)
+	secondval = val(c, d, p)
+	thirdval = val((a+c)/2, (b+d)/2, p)
+#	print(firstval + secondval)
+#	print (2*thirdval)
+#	print("Values are:")
+#	print(firstval, secondval, thirdval)
+	return firstval + secondval - 2*thirdval
+def ineq_test():
+	for _ in range(1000):
+		a = np.random.rand()+0.001
+		b = np.random.rand()+0.001
+		c = np.random.rand()+0.001
+		d = np.random.rand()+0.001
+		p = 10*np.random.rand()+0.1
+		p=1
+		s = inequality(a,b,c,d,p)
+		print(s)
+		if s <  -0.0000001:
+			print((a,b,c,d,p))
+			return False
+	print(p)
+	return True
+
+# Test for 3-entry diagonal matrix.
+def val_3(a, b, c, p):
+	av = pow(a, 1/p)
+	bv = pow(b, 1/p)
+	cv = pow(c, 1/p)
+	# Minimal value of ax^2 + by^2 + cz^2 when x+y+z = 1 
+	# ax = by = cz, x = 1/a, y = 1/b, z = 1/c, normalized
+	# value =1/(1/a+1/b+1/c)^2*(1/a+1/b+1/c)
+	v = 1/(1/a+1/b+1/c)
+	return pow(1/v, p)
+def inequality_3(a,b,c,d,e,f,p=3/2):
+	firstval = val_3(a,b,c,p)
+	secondval = val_3(d,e,f, p)
+	thirdval = val_3((a+d)/2, (b+e)/2, (c+f)/2, p)
+#	print(firstval + secondval)
+#	print (2*thirdval)
+#	print("Values are:")
+#	print(firstval, secondval, thirdval)
+	return firstval + secondval - 2*thirdval
+
+def three_ineq_test():
+	for _ in range(1000):
+		a = np.random.rand()+0.001
+		b = np.random.rand()+0.001
+		c = np.random.rand()+0.001
+		d = np.random.rand()+0.001
+		e = np.random.rand()+0.001
+		f = np.random.rand()+0.001
+		p = 4*np.random.rand()+0.5
+		q = 1
+		s = inequality_3(a,b,c,d,e,f,p)
+		sq = inequality_3(a,b,c,d,e,f,q)
+		t = inequality_3(a,a,a,a,a,b,p)
+		tq = inequality_3(a,a,a,a,a,b,q)
+		u = inequality_3(a,b,a,a,c,b,p)
+		uq = inequality_3(a,b,a,a,c,b,q)
+		if s <  -0.0000001:
+			print((a,b,c,d,e,f,p))
+			print(s)
+			return False
+		if sq <  -0.0000001:
+			print((a,b,c,d,e,f,q))
+			print(sq)
+			return False
+		if t < 0.0001:
+			print((a,a,a,a,a,b,p))
+			print("Failed on t")
+			print(t)
+			return False
+		if tq < -0.00000001:
+			print((a,a,a,a,a,b,q))
+			print("Failed on tq")
+			print(tq)
+			return False
+		if u < -0.00000001:
+			print((a,a,a,a,a,b,p))
+			print("Failed on u")
+			return False
+		if uq < -0.00000001:
+			print((a,a,a,a,a,b,q))
+			print("Failed on uq")
+			return False
+	print(p)
+	print(q)
+	return True
+
+# Testing whether negative type matrices are preserved under harmonic mean.	
+# They probably aren't. If the experiments show it, I'm probably not being thorough enough.
+
+def harmonic_mean(D1, D2):
+	return 2*inv(inv(D1)+inv(D2))
+def neg_type(D):
+	n = len(D)
+	P = np.zeros((n-1, n-1))
+	for i in range(1, n):
+		for j in range(1, n):
+			P[i-1][j-1] = D[0][i]+D[j][0]-D[i][j]
+	print( np.linalg.eig(P)[0])
+	sorted_real_eigvals =  np.sort(np.real(np.linalg.eig(P)[0]))
+	print(sorted_real_eigvals[0])
+	return sorted_real_eigvals[0] >-.000001
+
+
+def structured_n_dist(n):
+	x_size = int(n/2)
+	y_size = n-x_size
+	dim = n
+	x = np.random.rand(x_size,dim)
+	y = np.random.rand(y_size,dim)
+	for i in range(len(y)):
+		y[i][0] += 2
+	a = np.concatenate((x,y))
+	A = np.zeros((n,n))
+	for i in range(n):
+		for j in range(n):
+			A[i][j] = np.linalg.norm(a[i]-a[j])**2
+	return A
--- screw.py
+++ screw.py
@@ -26,24 +26,83 @@ def square_circumradius_change(alpha, l):
 	print("")
 	return circum_2_p - circum_1_p
 
-def hypothesis_test(l1, new_point, alpha=1.0/3, p = 2):
+# Tests whether the circumradius of the average of lists l1 and l2, is larger than the average of the circumradii.
+def general_convex_test(l1, l2, p):
+	l3 = [(l1[i]+l2[i])/2 for i in range(len(l1))]
+	diam_1 = diam_p(l1, p)
+	diam_2 = diam_p(l2, p)
+	diam_3 = diam_p(l3, p)
+	# I'm testing whether val is negative. If it's negative, my hypothesis is true, and if not, my hypothesis is false.
+	val = diam_1 + diam_2 - 2*diam_3
+	if np.random.rand() > 0.99:
+		print(diam_1)
+		print(val)
+	return val <= 0.000001
+
+def mass_general_convex_test(p):
+	mass = 1000
+	n = 10
+	for i in range(mass):
+		l1 = list(np.sort(np.random.rand(n)))
+		l2 = list(np.sort(np.random.rand(n)))
+		if not(general_convex_test(l1, l2, p)):
+			print((l1, l2))
+			return False
+	for i in range(mass):
+		l1 = list(np.linspace(0, n, n))+ [n+n*4*(i+1)/100]
+		l2 = list(np.sort(np.random.rand(n+1)))
+		if not(general_convex_test(l1, l2, p)):
+			print("Broke at: ")
+			print((l1, l2))
+			return False
+	l1 = [1,4,5,6,6.1,6.2, 9]
+	l2 = list(np.sort([-3,4,-10,5,6,1,1.1]))
+	if not(general_convex_test(l1, l2, p)):
+		return False
+	return True
+
+
+def convex_test(l1, final_point, p):
+	const_val = 300
+	final_points = np.linspace(l1[-1]+0.0001, final_point, const_val)
+	ds = (final_point-l1[-1])/const_val # Spacing for each test point.
+	for i in range(1, len(final_points)-1):
+		diam_mid = diam_p(l1+[final_points[i]], p)
+		diam_front = diam_p(l1+[final_points[i-1]], p)
+		diam_back = diam_p(l1+[final_points[i+1]], p)
+		print((diam_front, diam_back, diam_mid))
+		val = diam_front + diam_back - 2*diam_mid
+		print(val/(ds**2))
+		if val >= 0.000001:
+			return False
+	return True
+
+def hypothesis_test(l1, new_point, p=3):
+	alpha = 1/p
 	old_length = l1[-1] - l1[0]
 	new_length = new_point - l1[0]
 	l2 = l1 + [new_point]
-	circum_2_p = 2*rad_p(l2, alpha, p)
-	circum_1_p = 2*rad_p(l1, alpha, p)
+	circum_2_p = diam_p(l2, p)
+	circum_1_p = diam_p(l1, p)
 	circum_val = circum_2_p-circum_1_p
-	length_val = pow(pow(new_length, alpha), p) - pow(pow(old_length, alpha), p)
+	length_val = new_length - old_length
 	print(circum_val)
 	print(length_val)
-	print (circum_val - length_val)/(length_val)
+	print (circum_val - length_val)
+	print (circum_val / length_val)
+	print ((circum_val - length_val)/length_val)
 	return circum_val > length_val - eps
 
 def rad_alpha(ls, alpha=1/3):
 	return rad_p(ls, alpha, 1/alpha)
 
-def rad_p(ls, alpha=1/3, p = 3):
-	return pow(rad_squared(ls, alpha), p/2.0)
+def diam_p(ls, p=3):
+	ls = np.sort(ls)
+	alpha = 1/p
+	return diam_alpha_p(ls, alpha, p)
+
+def diam_alpha_p(ls, alpha=1/3, p = 3):
+	return pow(4*rad_squared(ls, alpha), p/2.0)
 
 def rad_squared(ls, alpha=1.0/3):
 	k = np.linalg.inv(cm(ls, 2*alpha))
@@ -111,7 +170,7 @@ def res(M, r):
 	return Mhat
 
 # Distance matrix squared, of screw with screw func |i-j|^power.
-def screw(ls, power=2.0/3):
+def screw(ls, power):
 	n = len(ls)
 	M = np.zeros((n, n))
 	for i in range(n):
